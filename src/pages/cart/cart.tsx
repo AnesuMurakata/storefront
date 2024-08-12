b87c9d7a0066a33a412a0d3601805c184c4557cb
AnesuMurakata <murac96@gmail.com>
2024-08-12 07:53:53 +0200

    add shopping cart total

--- src/pages/cart/cart.tsx
+++ src/pages/cart/cart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux';
 
@@ -9,13 +9,25 @@ import './cart.scss';
 import NavBar from '../../components/navBar/navBar';
 
 const Cart = () => {
+  const [shoppingCartTotal, setShoppingCartTotal] = useState(0);
   const cartProducts = useSelector((state: RootState) => state.cart.products);
+
+  useEffect(() => {
+    let totalCost = 0;
+    cartProducts.map((product) => {
+      totalCost += product.price;
+    });
+    setShoppingCartTotal(totalCost);
+  }, [cartProducts]);
 
   return (
     <div className="cart-container">
       <NavBar />
       <div className="cart-container__elements">
-        <h3>Shopping Cart</h3>
+        <div className="cart-container__elements__headers">
+          <h3>Shopping Cart</h3>
+          <h3>Total: ${shoppingCartTotal.toFixed(2)}</h3>
+        </div>
         <div className="cart-container__elements__cart-items-container">
           {cartProducts.map((product, index) => {
             return (
