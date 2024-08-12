b8354ee13009d08e7634c98b02cb07c1a71693ca
AnesuMurakata <murac96@gmail.com>
2024-08-12 07:42:33 +0200

    added cart items counter

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux';
 
 // STYLES
 import './navBar.scss';
 
 // ASSETS
 import search from '../../assets/components/navBar/search.png';
-import profile from '../../assets/components/navBar/profile.png';
-import likes from '../../assets/components/navBar/likes.png';
-import cart from '../../assets/components/navBar/shopping-cart.png';
+import cart from '../../assets/components/navBar/shopping-bag.png';
 
 const NavBar = () => {
   const navigate = useNavigate();
 
   const [displayResultsBar, setDisplayResultsBar] = useState(false);
+
+  const cartProducts = useSelector((state: RootState) => state.cart.products);
 
   return (
     <div className="nav-bar-container">
@@ -36,9 +38,8 @@ const NavBar = () => {
         )}
       </div>
       <div className="nav-bar-container__profile">
-        <img src={profile} alt="Account" />
-        <img src={likes} alt="Wish List" />
         <img src={cart} alt="Shopping Cart" onClick={() => navigate('/cart')} />
+        <p className="nav-bar-container__profile">{cartProducts.length}</p>
       </div>
     </div>
   );
